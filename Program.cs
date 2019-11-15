c58a62066fd6fa99c157ce6ab939ead3d2caad48
Dheeraj Swami <DSW015@crb.apmoller.net>
2019-11-15 14:18:40 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace SequenceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CloudStorageAccount account;
+            CloudTableClient client;
+            CloudTable table;
+
+            string accountName = "";
+            string accountKey = "";
+
+            if (accountName.Length == 0 || accountKey.Length == 0)
+            {
+                Console.WriteLine("Please provide account name and key");
+            }
+            else
+            {
+
+                account = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+                client = account.CreateCloudTableClient();
+                table = client.GetTableReference("Sequence");
+                SequenceGenerator sequenceGenerator = new SequenceGenerator();
+                //TO test if parallel request occurs
+                Parallel.Invoke
+                (
+                    async () => await sequenceGenerator.GenerateSequenceAsync(table).ConfigureAwait(false),
+                    async () => await sequenceGenerator.GenerateSequenceAsync(table).ConfigureAwait(false),
+                    async () => await sequenceGenerator.GenerateSequenceAsync(table).ConfigureAwait(false)
+                );
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+    public class SequenceGeneratorModel : TableEntity
+    {
+        public long SequenceId { get; set; }
+    }
+
+
+
+}
---
+++ SequenceGenerator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace SequenceGenerator
+{
+    public class SequenceGenerator
+    {
+        public async Task<long> GenerateSequenceAsync(CloudTable table)
+        {
+            SequenceGeneratorModel sequence = await GetSequenceRecordAsync(table).ConfigureAwait(false);
+
+            if (sequence == null)
+            {
+                sequence = new SequenceGeneratorModel
+                {
+                    PartitionKey = "default",
+                    RowKey = "rowId",
+                    SequenceId = 1
+                };
+                var insertOperation = TableOperation.Insert(sequence);
+                await table.ExecuteAsync(insertOperation).ConfigureAwait(false);
+            }
+            else
+            {
+                await RegenerateSequenceAsync(sequence, table).ConfigureAwait(false);
+            }
+
+            return sequence.SequenceId;
+        }
+
+        private async Task<SequenceGeneratorModel> GetSequenceRecordAsync(CloudTable table)
+        {
+            string partitionFilter = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "default");
+            string rowFilter = TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, "rowId");
+            string finalFilter = TableQuery.CombineFilters(partitionFilter, TableOperators.And, rowFilter);
+
+            TableQuery<SequenceGeneratorModel> query = new TableQuery<SequenceGeneratorModel>().Where(finalFilter);
+            var res = await table.ExecuteQuerySegmentedAsync<SequenceGeneratorModel>(query, null).ConfigureAwait(false);
+            return res.FirstOrDefault();
+        }
+
+        private async Task<SequenceGeneratorModel> RegenerateSequenceAsync(SequenceGeneratorModel sequence, CloudTable table)
+        {
+            try
+            {
+                sequence.SequenceId++;
+                TableResult tblr = await table.ExecuteAsync(TableOperation.InsertOrReplace(sequence), null, new OperationContext { UserHeaders = new Dictionary<string, string> { { "If-Match", sequence.ETag } } }).ConfigureAwait(false);
+            }
+            catch (StorageException ex)
+            {
+                ////"Optimistic concurrency violation – entity has changed since it was retrieved."
+                if (ex.RequestInformation.HttpStatusCode == 412)
+                {
+                    Thread.Sleep(300);
+                    sequence = await this.GetSequenceRecordAsync(table).ConfigureAwait(false);
+                    await this.RegenerateSequenceAsync(sequence, table).ConfigureAwait(false);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return sequence;
+        }
+    }
+}
